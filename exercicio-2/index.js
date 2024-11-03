6382416e759f02eb9eef60474e7c767bb15fafe3
Hernani Torres <hernanitorres87@gmail.com>
2024-11-02 23:28:40 -0300

    Exercício 2

---
+++ exercicio-2/index.js
@@ -0,0 +1,50 @@
+// Carro mais veloz //
+
+// let NameOfCar1 = prompt("Olá piloto, me informe o nome do seu carro:");
+// let VelocityCar1 = prompt("Piloto do carro: " + NameOfCar1 + "\ninsira o valor máximo de velocidade do seu carro:");
+// let NameOfCar2 = prompt("Olá piloto, me informe o nome do seu carro:");
+// let VelocityCar2 = prompt("Piloto do carro: " + NameOfCar2 + "\ninsira o valor máximo de velocidade do seu carro:");
+
+// if (VelocityCar1 > VelocityCar2) {
+//     alert (NameOfCar1 + " é mais rápido que o carro " + NameOfCar2)
+// } else if (VelocityCar1 < VelocityCar2) {
+//     alert (NameOfCar2 + " é mais rápido que o carro " + NameOfCar1)
+// } else {
+//     alert ("Ambos os carros possuem a mesma velocidade!")
+// };
+
+
+// Dano do personagem //
+
+let NameCharacterOne = prompt("Informe o nome do seu personagem:");
+let PowerCharacterOne = prompt("Informe o seu poder de ataque:");
+PowerCharacterOne = parseFloat(PowerCharacterOne)
+
+let NameCharacterTwo = prompt("Informe o nome do seu personagem:");
+let LifeOfPointsTwo = prompt("Informe a quantidade de pontos de vida:");
+LifeOfPointsTwo = parseFloat(LifeOfPointsTwo)
+let DefesePointsTwo = prompt("Informe a quantidade de pontos de defesa:");
+DefesePointsTwo = parseFloat(DefesePointsTwo)
+let QuestionOfShield = confirm("O seu personagem possui escudo?");
+let ResultPower = ""
+
+
+if (PowerCharacterOne > DefesePointsTwo && QuestionOfShield === false) {
+    ResultPower = PowerCharacterOne  - DefesePointsTwo
+    alert (NameCharacterTwo + " sofreu " + ResultPower + " pontos de dano!")
+} else if (PowerCharacterOne > DefesePointsTwo && QuestionOfShield === true ) {
+    ResultPower = (PowerCharacterOne - DefesePointsTwo) / 2
+    alert (NameCharacterTwo + " sofreu " + ResultPower + " pontos de dano!")
+} else if (PowerCharacterOne <= DefesePointsTwo) {
+    alert (NameCharacterTwo + " não sofreu dano!");
+};
+
+LifeOfPointsTwo -= ResultPower
+
+alert ("Informações atualizadas dos personagens:" +
+    "\n" + NameCharacterOne + "\nPoder de ataque: " + PowerCharacterOne + "\n\n" +
+    NameCharacterTwo + "\nPoder de defesa: " + DefesePointsTwo + "\nPontos de vida: " + LifeOfPointsTwo);
+
+
+
+
